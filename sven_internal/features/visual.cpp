8af0c0c120572adc40165cf32ccee65af6398963
Sw1ft <mgh.sw1ft@mail.ru>
2022-02-05 22:23:42 +0500

    Several options for ESP

--- sven_internal/features/visual.cpp
+++ sven_internal/features/visual.cpp
@@ -21,6 +21,7 @@
 #include "../game/usermsg.h"
 #include "../game/console.h"
 #include "../game/mathlib.h"
+#include "../game/player_utils.h"
 
 #include "../features/firstperson_roaming.h"
 
@@ -40,6 +41,7 @@ extern vgui::IPanel *g_pPanel;
 extern vgui::ISurface *g_pSurface;
 
 extern playermove_s *g_pPlayerMove;
+extern extra_player_info_t *g_pPlayerExtraInfo;
 
 //-----------------------------------------------------------------------------
 // Definitions
@@ -184,6 +186,7 @@ void CVisual::ESP()
 
 	int nLocalPlayer = pLocal->index;
 
+	// What a mess lol
 	for (int i = 1; i <= MAXENTS; ++i)
 	{
 		if (i == nLocalPlayer)
@@ -193,10 +196,9 @@ void CVisual::ESP()
 
 		studiohdr_t *pStudioHeader = NULL;
 		model_s *pModel = NULL;
-
 		class_info_t classInfo = { CLASS_PLAYER, FL_CLASS_FRIEND };
 
-		if (!pEntity || !(pModel = pEntity->model) || *pModel->name != 'm' || (!pEntity->player && pEntity->curstate.solid <= 1) || pEntity->curstate.movetype == MOVETYPE_NONE)
+		if (!pEntity || !(pModel = pEntity->model) || *pModel->name != 'm' || (!pEntity->player && pEntity->curstate.solid <= SOLID_TRIGGER) || pEntity->curstate.movetype == MOVETYPE_NONE)
 			continue;
 
 		if (pEntity == pViewModel || pEntity->curstate.messagenum < pLocal->curstate.messagenum || pEntity->curstate.maxs.z == 0.0f && pEntity->curstate.mins.z == 0.0f)
@@ -205,9 +207,10 @@ void CVisual::ESP()
 		if (!(pStudioHeader = (studiohdr_t *)g_pEngineStudio->Mod_Extradata(pModel)) || pStudioHeader->numhitboxes <= 3)
 			continue;
 
+		bool bPlayer = pEntity->player;
 		float flDistance = (pEntity->origin - pLocal->origin).Length();
 
-		if (flDistance > 3000.0f)
+		if (flDistance > 4000.0f)
 			continue;
 
 		float vecScreenBottom[2], vecScreenTop[2];
@@ -215,15 +218,19 @@ void CVisual::ESP()
 		Vector vecBottom = pEntity->origin;
 		Vector vecTop = pEntity->origin;
 
-		if (!pEntity->player)
+		if (!bPlayer)
 		{
 			classInfo = GetEntityClassInfo(pModel->name);
 
-			if (IsEntityClassDeadbody(classInfo, pEntity->curstate.solid) || IsEntityClassTrash(classInfo)) // don't process dead bodies and trash
+			// Don't process if entity isn't an ESP's target, or its dead body or trash
+			if (g_Config.cvars.esp_targets == 2 || IsEntityClassDeadbody(classInfo, pEntity->curstate.solid) || IsEntityClassTrash(classInfo))
 				continue;
 		}
 		else
 		{
+			if (g_Config.cvars.esp_targets == 1)
+				continue;
+
 			vecBottom.z -= pEntity->curstate.maxs.z;
 		}
 
@@ -243,7 +250,7 @@ void CVisual::ESP()
 
 			float boxHeight = vecScreenTop[1] - vecScreenBottom[1];
 
-			if (pEntity->player)
+			if (bPlayer)
 			{
 				if (pEntity->curstate.usehull)
 					boxHeight *= 0.9985f;
@@ -290,7 +297,7 @@ void CVisual::ESP()
 
 			if (g_Config.cvars.esp_box_index)
 			{
-				int y = int(vecScreenBottom[1] + (8.f + boxHeight));
+				int y = int(vecScreenBottom[1] + (boxHeight / 2));
 				g_Drawing.DrawStringF(g_hESP, int(vecScreenBottom[0]), y, 255, 255, 255, 255, FONT_ALIGN_CENTER, "%d", i);
 			}
 			
@@ -300,12 +307,47 @@ void CVisual::ESP()
 				g_Drawing.DrawStringF(g_hESP, int(vecScreenBottom[0]), y, 255, 255, 255, 255, FONT_ALIGN_CENTER, "%.1f", flDistance);
 			}
 
-			if (pEntity->player)
+			if (bPlayer)
 			{
+				if (g_Config.cvars.esp_box_player_health)
+				{
+					int iHealth;
+
+					if ( ( iHealth = int(GetPlayerHealth(i)) ) != 0 )
+					{
+						if (iHealth == -1)
+							iHealth = 0;
+						else if (iHealth > 100)
+							iHealth = 100;
+
+						int y = int(vecScreenBottom[1] + (boxHeight - 8.0f));
+
+						g_Drawing.DrawStringF(g_hESP,
+											  int(vecScreenBottom[0]),
+											  y,
+											  int( 255.f * (iHealth > 50 ? 1.f - 2.f * (iHealth - 50) / 100.f : 1.f) ),
+											  int( 255.f * ((iHealth > 50 ? 1.f : 2.f * iHealth / 100.f)) ),
+											  0,
+											  255,
+											  FONT_ALIGN_CENTER,
+											  "%d",
+											  iHealth);
+					}
+				}
+
+				if (g_Config.cvars.esp_box_player_armor)
+				{
+					float flArmor = GetPlayerArmor(i);
+					int y = int(vecScreenBottom[1] + (boxHeight + 8.f));
+
+					if (flArmor >= 0.f)
+						g_Drawing.DrawStringF(g_hESP, int(vecScreenBottom[0]), y, 153, 191, 255, 255, FONT_ALIGN_CENTER, "%.1f", flArmor);
+				}
+
 				if (g_Config.cvars.esp_box_player_name)
 				{
 					int y = int(vecScreenTop[1] + (8.f - boxHeight));
-					player_info_s * pPlayer = g_pEngineStudio->PlayerInfo(i - 1);
+					player_info_s *pPlayer = g_pEngineStudio->PlayerInfo(i - 1);
 
 					g_Drawing.DrawStringF(g_hESP, int(vecScreenBottom[0]), y, r, g, b, 255, FONT_ALIGN_CENTER, "%s", pPlayer->name);
 				}
@@ -326,7 +368,7 @@ void CVisual::ESP()
 			}
 
 		#ifdef PROCESS_PLAYER_BONES_ONLY
-			if ((g_Config.cvars.esp_skeleton || g_Config.cvars.esp_bones_name) && pEntity->player)
+			if ((g_Config.cvars.esp_skeleton || g_Config.cvars.esp_bones_name) && bPlayer)
 		#else
 			if (g_Config.cvars.esp_skeleton || g_Config.cvars.esp_bones_name)
 		#endif
