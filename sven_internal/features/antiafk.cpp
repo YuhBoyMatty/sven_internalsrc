26934696ad4ea65414cd2422c569974cf0cb7ff5
Sw1ft <mgh.sw1ft@mail.ru>
2022-02-06 20:43:45 +0500

    AntiAFK: stay within a range

--- sven_internal/features/antiafk.cpp
+++ sven_internal/features/antiafk.cpp
@@ -8,19 +8,27 @@
 #include "../game/utils.h"
 #include "../game/console.h"
 #include "../game/mathlib.h"
+#include "../game/player_utils.h"
+
 #include "../config.h"
 
+//-----------------------------------------------------------------------------
+// Imports
 //-----------------------------------------------------------------------------
 
 extern playermove_s *g_pPlayerMove;
 
 //-----------------------------------------------------------------------------
+// Vars
+//-----------------------------------------------------------------------------
 
 CAntiAFK g_AntiAFK;
 
 //-----------------------------------------------------------------------------
+// ConCommands
+//-----------------------------------------------------------------------------
 
-CON_COMMAND_FUNC(sc_antiafk, ConCommand_AntiAFK, "sc_antiafk [mode] - Set Anti-AFK Mode")
+CON_COMMAND_FUNC(sc_antiafk, ConCommand_AntiAFK, "sc_antiafk [mode] - Set Anti-AFK Mode [0-5]")
 {
 	if (CMD_ARGC() >= 2)
 	{
@@ -30,6 +38,11 @@ CON_COMMAND_FUNC(sc_antiafk, ConCommand_AntiAFK, "sc_antiafk [mode] - Set Anti-A
 		{
 			g_Config.cvars.antiafk = nMode;
 		}
+		else
+		{
+			Msg("Wrong Anti-AFK mode\n");
+			Msg("Available modes:\n0 - Off\n1 - Step Forward & Back\n2 - Spam Gibme\n3 - Walk Around & Spam Inputs\n4 - Walk Around\n5 - Go Right\n");
+		}
 	}
 	else
 	{
@@ -37,101 +50,40 @@ CON_COMMAND_FUNC(sc_antiafk, ConCommand_AntiAFK, "sc_antiafk [mode] - Set Anti-A
 	}
 }
 
-void RotateCamera()
+//-----------------------------------------------------------------------------
+// Anti-AFK implementations
+//-----------------------------------------------------------------------------
+
+CAntiAFK::CAntiAFK()
 {
-	Vector va;
+	m_bDead = true;
+	m_bComingBackToAFKPoint = false;
 
-	float flRotationAngle = g_Config.cvars.antiafk_rotation_angle;
-	float flRotationAngleAbs = fabsf(flRotationAngle);
+	m_vecAFKPoint.x = 0.f;
+	m_vecAFKPoint.y = 0.f;
 
-	static float s_flPitchDirection = 1.0f;
-
-	g_pEngineFuncs->GetViewAngles(va);
-
-	if (s_flPitchDirection > 0.0f)
-	{
-		if (va.x + flRotationAngleAbs > 89.0f)
-			s_flPitchDirection = -1.0f;
-		else
-			va.x += flRotationAngleAbs;
-	}
-
-	if (s_flPitchDirection < 0.0f)
-	{
-		if (va.x - flRotationAngleAbs < -89.0f)
-			s_flPitchDirection = 1.0f;
-		else
-			va.x -= flRotationAngleAbs;
-	}
-
-	va.y += flRotationAngle;
-	va.y = NormalizeAngle(va.y);
-
-	g_pEngineFuncs->SetViewAngles(va);
+	m_flComingBackStartTime = -1.f;
 }
 
-void WalkAround(struct usercmd_s *cmd, int &delay, int &movement_button, const int delay_count)
+void CAntiAFK::Reset()
 {
-REPEAT:
-	if (movement_button == IN_MOVERIGHT)
-	{
-		if (delay > delay_count)
-		{
-			movement_button = IN_BACK;
-			delay = 0;
+	m_bDead = true;
+	m_bComingBackToAFKPoint = false;
 
-			goto REPEAT;
-		}
-		else
-		{
-			cmd->sidemove = g_pPlayerMove->clientmaxspeed;
-		}
-	}
-	else if (movement_button == IN_BACK)
-	{
-		if (delay > delay_count)
-		{
-			movement_button = IN_MOVELEFT;
-			delay = 0;
+	m_vecAFKPoint.x = 0.f;
+	m_vecAFKPoint.y = 0.f;
 
-			goto REPEAT;
-		}
-		else
-		{
-			cmd->forwardmove = -g_pPlayerMove->clientmaxspeed;
-		}
-	}
-	else if (movement_button == IN_MOVELEFT)
-	{
-		if (delay > delay_count)
-		{
-			movement_button = IN_FORWARD;
-			delay = 0;
+	m_flComingBackStartTime = -1.f;
+}
 
-			goto REPEAT;
-		}
-		else
-		{
-			cmd->sidemove = -g_pPlayerMove->clientmaxspeed;
-		}
-	}
-	else if (movement_button == IN_FORWARD)
-	{
-		if (delay > delay_count)
-		{
-			movement_button = IN_MOVERIGHT;
-			delay = 0;
+void CAntiAFK::OnHUDInit()
+{
+	Reset();
+}
 
-			goto REPEAT;
-		}
-		else
-		{
-			cmd->forwardmove = g_pPlayerMove->clientmaxspeed;
-		}
-	}
-
-	cmd->buttons |= movement_button;
-	++delay;
+void CAntiAFK::OnVideoInit()
+{
+	Reset();
 }
 
 void CAntiAFK::CreateMove(float frametime, struct usercmd_s *cmd, int active)
@@ -139,12 +91,135 @@ void CAntiAFK::CreateMove(float frametime, struct usercmd_s *cmd, int active)
 	AntiAFK(cmd);
 }
 
+void CAntiAFK::OnRevive()
+{
+	if (!g_pPlayerMove)
+	{
+		m_bDead = true;
+		return;
+	}
+
+	m_vecAFKPoint.x = g_pPlayerMove->origin.x;
+	m_vecAFKPoint.y = g_pPlayerMove->origin.y;
+
+	m_bDead = false;
+	m_bComingBackToAFKPoint = false;
+
+	m_flComingBackStartTime = -1.f;
+}
+
+void CAntiAFK::OnDie()
+{
+	m_bDead = true;
+	m_bComingBackToAFKPoint = false;
+
+	m_vecAFKPoint.x = 0.f;
+	m_vecAFKPoint.y = 0.f;
+
+	m_flComingBackStartTime = -1.f;
+}
+
 void CAntiAFK::AntiAFK(struct usercmd_s *cmd)
 {
 	int nMode = g_Config.cvars.antiafk;
 
 	if (!nMode)
+	{
+		m_flComingBackStartTime = -1.0f;
+		m_bComingBackToAFKPoint = false;
+		m_bDead = true;
 		return;
+	}
+
+	bool bDead = !IsLocalPlayerAlive();
+	
+	if (bDead != m_bDead)
+	{
+		if (bDead && !m_bDead)
+			OnDie();
+		else
+			OnRevive();
+	}
+
+	if (bDead)
+		return;
+
+	if (g_Config.cvars.antiafk_stay_within_range)
+	{
+		if (m_vecAFKPoint.x == 0.0f && m_vecAFKPoint.y == 0.0f)
+			m_vecAFKPoint = g_pPlayerMove->origin.Make2D();
+
+		Vector2D vecOrigin = g_pPlayerMove->origin.Make2D();
+		float flDistanceToAFKPointSqr = (m_vecAFKPoint - vecOrigin).LengthSqr();
+
+		if (m_bComingBackToAFKPoint || flDistanceToAFKPointSqr > M_SQR(g_Config.cvars.antiafk_stay_radius)) // moved out of range
+		{
+			if (m_flComingBackStartTime == -1.0f)
+				m_flComingBackStartTime = g_pEngineFuncs->GetClientTime();
+			// Coming to the AFK point too long, reset current state
+			else if (g_pEngineFuncs->GetClientTime() - m_flComingBackStartTime >= 10.f)
+				Reset();
+
+			if (m_bComingBackToAFKPoint && flDistanceToAFKPointSqr <= M_SQR(25.0f))
+			{
+				m_bComingBackToAFKPoint = false;
+				return;
+			}
+
+			Vector2D vecForward;
+			Vector2D vecRight;
+
+			Vector2D vecDir = (m_vecAFKPoint - vecOrigin).Normalize();
+
+			m_bComingBackToAFKPoint = true;
+
+			// Rotate the wish vector by a random direction, must help if we stuck somewhere
+			int nRandom = g_pEngineFuncs->pfnRandomLong(0, 1);
+
+			float flTheta = g_Config.cvars.antiafk_stay_radius_offset_angle * static_cast<float>(M_PI) / 180.0f;
+
+			float ct = cosf(flTheta);
+			float st = sinf(flTheta);
+
+			if (nRandom % 2) // counter clockwise
+			{
+				vecDir.x = vecDir.x * ct - vecDir.y * st;
+				vecDir.y = vecDir.x * st + vecDir.y * ct;
+			}
+			else // clockwise
+			{
+				vecDir.x = vecDir.x * ct + vecDir.y * st;
+				vecDir.y = -vecDir.x * st + vecDir.y * ct;
+			}
+
+			// Forward angles
+			vecForward.x = cosf(cmd->viewangles.y * static_cast<float>(M_PI) / 180.f);
+			vecForward.y = sinf(cmd->viewangles.y * static_cast<float>(M_PI) / 180.f);
+
+			// Make a right vector of angles. Rotate forward vector as a complex number by 90 deg.
+			vecRight.x = vecForward.y;
+			vecRight.y = -vecForward.x;
+
+			// Multiply by max movement speed
+			vecForward = vecForward * g_pPlayerMove->clientmaxspeed;
+			vecRight = vecRight * g_pPlayerMove->clientmaxspeed;
+
+			// Project onto direction vector
+			float forwardmove = DotProduct(vecForward, vecDir);
+			float sidemove = DotProduct(vecRight, vecDir);
+
+			// Apply moves
+			cmd->forwardmove = forwardmove;
+			cmd->sidemove = sidemove;
+
+			return;
+		}
+	}
+	else
+	{
+		m_bComingBackToAFKPoint = false;
+		m_flComingBackStartTime = -1.0f;
+	}
 
 	if (nMode == 1)
 	{
@@ -226,6 +301,107 @@ void CAntiAFK::AntiAFK(struct usercmd_s *cmd)
 	}
 }
 
+void CAntiAFK::RotateCamera()
+{
+	if (g_Config.cvars.antiafk_rotate_camera)
+	{
+		Vector va;
+
+		float flRotationAngle = g_Config.cvars.antiafk_rotation_angle;
+		float flRotationAngleAbs = fabsf(flRotationAngle);
+
+		static float s_flPitchDirection = 1.0f;
+
+		g_pEngineFuncs->GetViewAngles(va);
+
+		if (s_flPitchDirection > 0.0f)
+		{
+			if (va.x + flRotationAngleAbs > 89.0f)
+				s_flPitchDirection = -1.0f;
+			else
+				va.x += flRotationAngleAbs;
+		}
+		else if (s_flPitchDirection < 0.0f)
+		{
+			if (va.x - flRotationAngleAbs < -89.0f)
+				s_flPitchDirection = 1.0f;
+			else
+				va.x -= flRotationAngleAbs;
+		}
+
+		va.y += flRotationAngle;
+		va.y = NormalizeAngle(va.y);
+
+		g_pEngineFuncs->SetViewAngles(va);
+	}
+}
+
+void CAntiAFK::WalkAround(struct usercmd_s *cmd, int &delay, int &movement_button, const int delay_count)
+{
+REPEAT:
+	if (movement_button == IN_MOVERIGHT)
+	{
+		if (delay > delay_count)
+		{
+			movement_button = IN_BACK;
+			delay = 0;
+
+			goto REPEAT;
+		}
+		else
+		{
+			cmd->sidemove = g_pPlayerMove->clientmaxspeed;
+		}
+	}
+	else if (movement_button == IN_BACK)
+	{
+		if (delay > delay_count)
+		{
+			movement_button = IN_MOVELEFT;
+			delay = 0;
+
+			goto REPEAT;
+		}
+		else
+		{
+			cmd->forwardmove = -g_pPlayerMove->clientmaxspeed;
+		}
+	}
+	else if (movement_button == IN_MOVELEFT)
+	{
+		if (delay > delay_count)
+		{
+			movement_button = IN_FORWARD;
+			delay = 0;
+
+			goto REPEAT;
+		}
+		else
+		{
+			cmd->sidemove = -g_pPlayerMove->clientmaxspeed;
+		}
+	}
+	else if (movement_button == IN_FORWARD)
+	{
+		if (delay > delay_count)
+		{
+			movement_button = IN_MOVERIGHT;
+			delay = 0;
+
+			goto REPEAT;
+		}
+		else
+		{
+			cmd->forwardmove = g_pPlayerMove->clientmaxspeed;
+		}
+	}
+
+	cmd->buttons |= movement_button;
+	++delay;
+}
+
+//-----------------------------------------------------------------------------
+// Initialize
 //-----------------------------------------------------------------------------
 
 void CAntiAFK::Init()
--- sven_internal/features/antiafk.h
+++ sven_internal/features/antiafk.h
@@ -2,15 +2,34 @@
 
 #pragma once
 
+#include "../sdk.h"
+
 class CAntiAFK
 {
 public:
-	void Init();
+	CAntiAFK();
 
+	void Init();
+	void Reset();
+
+	void OnHUDInit();
+	void OnVideoInit();
 	void CreateMove(float frametime, struct usercmd_s *cmd, int active);
 
 private:
 	void AntiAFK(struct usercmd_s *cmd);
+	void WalkAround(struct usercmd_s *cmd, int &delay, int &movement_button, const int delay_count);
+	void RotateCamera();
+
+	void OnRevive();
+	void OnDie();
+
+private:
+	bool m_bDead;
+	bool m_bComingBackToAFKPoint;
+
+	Vector2D m_vecAFKPoint; // 2D point only
+	float m_flComingBackStartTime;
 };
 
 extern CAntiAFK g_AntiAFK;
